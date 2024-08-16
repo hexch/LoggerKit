4cd874efce39ea5b364e8ce69706970895d83f41
hexch <hexch.c@gmail.com>
2024-08-16 19:49:31 +0900

    add logger class

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "LoggerKit",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.macOS(.v10_15), .iOS(.v14), .tvOS(.v14), .watchOS(.v7), .macCatalyst(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
--- Sources/LoggerKit/LoggerKit.swift
+++ Sources/LoggerKit/LoggerKit.swift
@@ -1,2 +1,43 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Logger.swift
+//  SharedLoggerKit
+//
+//  Created by XIAOCHUAN HE on R 5/06/21.
+//
+
+import Foundation
+import os
+fileprivate let kBundleID = Bundle.main.bundleIdentifier!
+fileprivate let kTagColMax = 8
+
+public enum Log:String{
+    case base
+    
+    // UI >>
+    // add ui log tag here
+    // << UI
+}
+
+@available(iOS 14.0, *)
+public extension Log{
+    var category:String {
+        self.rawValue
+    }
+    var logger: Logger{
+        Logger(subsystem: kBundleID, category: self.category)
+    }
+    func debug(_ tag: String, _ msg: String){
+        #if DEBUG
+        logger.debug("\("[\(tag)]", align: .left(columns: kTagColMax), privacy: .public)\(msg, privacy: .public)")
+        #else
+        logger.debug("\("[\(tag)]", align: .left(columns: kTagColMax), privacy: .public)\(msg, privacy: .private)")
+        #endif
+    }
+    
+    func info(_ tag: String, _ msg: String){
+        logger.info("\("[\(tag)]", align: .left(columns: kTagColMax), privacy: .public)\(msg, privacy: .public)")
+    }
+    func error(_ tag: String, _ msg: String){
+        logger.error("\("[\(tag)]", align: .left(columns: kTagColMax), privacy: .public)\(msg, privacy: .public)")
+    }
+}
