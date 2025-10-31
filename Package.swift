02672872e663f9913e7fd19767a0a79d2db3e87c
hexch <hexch.c@gmail.com>
2025-10-31 15:25:45 +0900

    iOS minimum support version: iOS 14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "LoggerKit",
-    platforms: [.macOS(.v14), .iOS(.v12), .tvOS(.v14), .watchOS(.v7), .macCatalyst(.v14)],
+    platforms: [.macOS(.v14), .iOS(.v14), .tvOS(.v14), .watchOS(.v7), .macCatalyst(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
